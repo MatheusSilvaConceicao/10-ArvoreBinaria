487fd91486ca1a15dfc386b38bf11ab9d1908929
Matheus Fernandes <100230174+MatheusSilvaConceicao@users.noreply.github.com>
2025-04-22 11:29:12 -0300

    Update ArvoreBinaria.cpp

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 // definicao de tipo
 struct NO {
-	int valor;
-	NO* esq;
-	NO* dir;
+    int valor;
+    NO* esq;
+    NO* dir;
 };
 
 NO* raiz = NULL;
@@ -18,139 +18,136 @@ void inserir();
 void exibir();
 void exibirQuantidade();
 
-
-
 // funcoes auxiliares Arvore
 NO* insereArvore(NO* no, int valor);
 NO* criaNO(int valor);
 int elementosArvore(NO* no);
 void exibirElementosArvore(NO* no);
-//--------------------------
 
+//--------------------------
 
 int main()
 {
-	menu();
+    menu();
 }
 
 void menu()
 {
-	int op = 0;
-	while (op != 5) {
-		system("cls"); // somente no windows
-		cout << "Menu Arvore";
-		cout << endl << endl;
-		cout << "1 - Inicializar Arvore \n";
-		cout << "2 - Exibir quantidade de elementos \n";
-		cout << "3 - Inserir elemento \n";
-		cout << "4 - Exibir elementos \n";
+    int op = 0;
+    while (op != 5) {
+        system("cls"); // somente no windows
+        cout << "Menu Arvore";
+        cout << endl << endl;
+        cout << "1 - Inicializar Arvore \n";
+        cout << "2 - Exibir quantidade de elementos \n";
+        cout << "3 - Inserir elemento \n";
+        cout << "4 - Exibir elementos \n";
+        cout << "5 - Sair \n\n";
 
-		cout << "5 - Sair \n";
+        cout << "Opcao: ";
+        cin >> op;
 
-		cout << "Opcao: ";
-		cin >> op;
+        switch (op)
+        {
+        case 1: inicializar();
+            break;
+        case 2: exibirQuantidade();
+            break;
+        case 3: inserir();
+            break;
+        case 4: exibir();
+            break;
+        case 5: return;
+        default: break;
+        }
 
-		switch (op)
-		{
-		case 1: inicializar();
-			break;
-		case 2:exibirQuantidade();
-			break;
-		case 3: inserir();
-			break;
-		case 4: exibir();
-			break;
-		default:
-			break;
-		}
-
-		system("pause"); // somente no windows
-	}
+        system("pause"); // somente no windows
+    }
 }
 
 void inicializar()
 {
-
-	// provisório porque não libera a memoria usada pela arvore
-	NO* raiz = NULL;
-	
-	cout << "Arvore inicializada \n";
-
+    // provisÃ³rio porque nÃ£o libera a memoria usada pela arvore
+    raiz = NULL;
+    cout << "Arvore inicializada \n";
 }
-
 
 void inserir()
 {
-	int valor;
-	cout << "Digite o elemento: ";
-	cin >> valor;
-	if (raiz == NULL) {
-		raiz = criaNO(valor);
-	}
-	else {
-		 insereArvore(raiz, valor);
-	}
-
-
+    int valor;
+    cout << "Digite o elemento: ";
+    cin >> valor;
+    if (raiz == NULL) {
+        raiz = criaNO(valor);
+    }
+    else {
+        insereArvore(raiz, valor);
+    }
 }
 
 void exibirQuantidade() {
-	cout << "Quantidade de elementos: " << elementosArvore(raiz) << endl;
-	
+    cout << "Quantidade de elementos: " << elementosArvore(raiz) << endl;
 }
 
 void exibir() {
-	exibirElementosArvore(raiz);
+    cout << "Elementos da Ã¡rvore (em ordem): ";
+    exibirElementosArvore(raiz);
+    cout << endl;
 }
-
 
 NO* criaNO(int valor)
 {
-	NO* novo = (NO*)malloc(sizeof(NO));
-	if (novo == NULL)
-	{
-		return NULL;
-	}
+    NO* novo = (NO*)malloc(sizeof(NO));
+    if (novo == NULL)
+    {
+        return NULL;
+    }
 
-	novo->valor = valor;
-	novo->esq = NULL;
-	novo->dir = NULL;
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
 
-	return novo;
+    return novo;
 }
 
 NO* insereArvore(NO* no, int valor)
 {
-	if (no->valor > valor && no->esq == NULL) {
-		no->esq = criaNO(valor);
-		return no->esq;
-	}
-	else if (no->valor < valor && no->dir == NULL) {
-		no->dir = criaNO(valor);
-		return no->dir;
-	}
-	else if (no->valor > valor) {
-		return insereArvore(no->esq, valor);
-	}
-	else if (no->valor < valor) {
-		return insereArvore(no->dir, valor);
-	}
-	else {
-		return NULL;
-	}
-	
+    if (no->valor > valor && no->esq == NULL) {
+        no->esq = criaNO(valor);
+        return no->esq;
+    }
+    else if (no->valor < valor && no->dir == NULL) {
+        no->dir = criaNO(valor);
+        return no->dir;
+    }
+    else if (no->valor > valor) {
+        return insereArvore(no->esq, valor);
+    }
+    else if (no->valor < valor) {
+        return insereArvore(no->dir, valor);
+    }
+    else {
+       
+        return NULL;
+    }
 }
 
 int elementosArvore(NO* no)
 {
-	if (no == NULL) {
-		return 0;
-	}
+    if (no == NULL) {
+        return 0;
+    }
 
-	return 1 + elementosArvore(no->esq) + elementosArvore(no->dir);
+    return 1 + elementosArvore(no->esq) + elementosArvore(no->dir);
 }
 
 void exibirElementosArvore(NO* no)
 {
-	
+    if (no == NULL) {
+        return;
+    }
+
+    exibirElementosArvore(no->esq); 
+    cout << no->valor << " ";         
+    exibirElementosArvore(no->dir);   
 }
